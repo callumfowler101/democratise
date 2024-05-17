49f7c92a6c50e912cf8baba21ec46cf0bb15d5fc
Callum Fowler <callumfowler@Callums-MacBook-Pro.local>
2024-05-17 09:33:13 +0100

    add question and num of resp callbacks

--- app.js
+++ app.js
@@ -1,43 +1,84 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN
-})
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
 
-app.message('hello', async ({message, say})=> {
-    await say({
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `Hey there <@${message.user}>`
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Click Me"
-                    },
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text: `Hey there <@${message.user}>`
-    })
-}) 
+const ballotObject = {
+  question: "",
+  numOfAnswers: 0,
+  answers: [],
+  completionDate: "",
+  team: "",
+  mandatory: "",
+};
 
-app.action('button_click', async ({body, ack, say}) => {
-    await ack()
-    await say(`<@${body.user.id}> cliked the button`)
-})
+app.message("hello", async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        dispatch_action: true,
+        type: "input",
+        element: {
+          type: "plain_text_input",
+          action_id: "question_log",
+        },
+        label: {
+          type: "plain_text",
+          text: "Question",
+          emoji: true,
+        },
+      },
+    ],
+    text: "fallback",
+  });
+});
+
+app.action("question_log", async ({ body, ack, say }) => {
+  await ack();
+
+  const blockId = body.message.blocks[0]["block_id"];
+  const _question = body.state.values[`${blockId}`]["question_log"].value;
+  ballotObject.question = _question;
+  await say({
+    blocks: [
+      {
+        dispatch_action: true,
+        type: "input",
+        element: {
+          type: "plain_text_input",
+          action_id: "num_of_responses_log",
+        },
+        label: {
+          type: "plain_text",
+          text: "Number of Responses",
+          emoji: true,
+        },
+      },
+    ],
+    text: "fallback",
+  });
+});
+
+app.action("num_of_responses_log", async ({ body, ack, say }) => {
+  await ack();
+
+  const blockId = body.message.blocks[0]["block_id"];
+  const _numOfResponses = Number(
+    body.state.values[`${blockId}`]["num_of_responses_log"].value
+  );
+
+  if (_numOfResponses === NaN) {
+    await say({ text: "Please enter a number" });
+  }
+});
 
 const main = async () => {
-    await app.start(process.env.PORT || 3000)
-    console.log('app is running')
-}
+  await app.start(process.env.PORT || 3000);
+  console.log("app is running");
+};
 
-main()
+main();
