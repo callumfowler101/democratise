edc361d04e9d3d982cace6d153bf8208344b916c
Callum Fowler <callumfowler@callums-mbp.home>
2024-05-20 18:12:48 +0100

    add genAnwsBlcksFnc, parseResFnc

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-const { App } = require("@slack/bolt");
+const { App } = require("@slack/bolt")
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-});
+})
 
 const ballotObject = {
   question: "",
   numOfAnswers: 0,
   answers: [],
   completionDate: "",
+  additionalThresh: 0,
   team: "",
   mandatory: "",
-};
+}
+
+const generateAnswerBlocks = (numOfAnswers) => {
+  const answersBlock = []
+  for (let i = 0; i < numOfAnswers; i++) {
+    answersBlock.push({
+      type: "input",
+      element: {
+        type: "plain_text_input",
+        action_id: "answers_log",
+      },
+      label: {
+        type: "plain_text",
+        text: `Answer ${i + 1}`,
+        emoji: true,
+      },
+    })
+  }
+  answersBlock[answersBlock.length - 1]["dispatch_action"] = true
+  return answersBlock
+}
+
+const parseResults = (values, actionKey) => {
+  const results = []
+  Object.values(values).forEach((e) => {
+    results.push(e[actionKey].value)
+  })
+  return results
+}
 
 app.message("hello", async ({ message, say }) => {
   await say({
@@ -34,15 +63,14 @@ app.message("hello", async ({ message, say }) => {
       },
     ],
     text: "fallback",
-  });
-});
+  })
+})
 
 app.action("question_log", async ({ body, ack, say }) => {
-  await ack();
+  await ack()
 
-  const blockId = body.message.blocks[0]["block_id"];
-  const _question = body.state.values[`${blockId}`]["question_log"].value;
-  ballotObject.question = _question;
+  const _question = parseResults(body.state.values, "question_log")[0]
+  ballotObject.question = _question
   await say({
     blocks: [
       {
@@ -50,35 +78,37 @@ app.action("question_log", async ({ body, ack, say }) => {
         type: "input",
         element: {
           type: "plain_text_input",
-          action_id: "num_of_responses_log",
+          action_id: "num_of_answers_log",
         },
         label: {
           type: "plain_text",
-          text: "Number of Responses",
+          text: "Number of Answers",
           emoji: true,
         },
       },
     ],
     text: "fallback",
-  });
-});
+  })
+})
 
-app.action("num_of_responses_log", async ({ body, ack, say }) => {
-  await ack();
+app.action("num_of_answers_log", async ({ body, ack, say }) => {
+  await ack()
+  const _numOfAnswers = parseResults(body.state.values, "num_of_answers_log")[0]
 
-  const blockId = body.message.blocks[0]["block_id"];
-  const _numOfResponses = Number(
-    body.state.values[`${blockId}`]["num_of_responses_log"].value
-  );
+  ballotObject.numOfAnswers = _numOfAnswers
+  const blocks = generateAnswerBlocks(ballotObject.numOfAnswers)
+  await say({ blocks, text: "fallback" })
+})
 
-  if (_numOfResponses === NaN) {
-    await say({ text: "Please enter a number" });
-  }
-});
+app.action("answers_log", async ({ body, ack, say }) => {
+  await ack()
+  const _answersArr = parseResults(body.state.values, "answers_log")
+  console.log(_answersArr)
+})
 
 const main = async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log("app is running");
-};
+  await app.start(process.env.PORT || 3000)
+  console.log("app is running")
+}
 
-main();
+main()
