94255cc82c0b212626031ac6c0e049b1ad2e4e13
Callum Fowler <callumfowler@callums-mbp.home>
2024-05-21 19:34:32 +0100

    remove mandatory field

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const ballotObject = {
   completionDate: "",
   additionalThresh: 0,
   channel: "",
-  mandatory: "",
 }
 
 const generateBlock = (title, blockType, actionId, firesAction = false) => {
